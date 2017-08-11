e0ee28951eb1706772f256c261129feaa7523d65
Edin Mujagic <edinmujagic2@gmail.com>
2017-08-12 01:55:47 +0200

    Code style changes

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 var net = require('net');
 
-module.exports = function(port, done) {
+module.exports = function (port, done) {
     done = done || port;
-    if(typeof port === 'function') {
+    if (typeof port === 'function') {
         port = 0;
     }
 
     var server = net.createServer(),
         _port = -1;
 
-    server.on('listening', function() {
+    server.on('listening', function () {
         _port = server.address().port;
         server.close();
     });
 
-    server.on('close', function() {
+    server.on('close', function () {
         done(server.has_error, _port);
     });
 
-    server.on('error', function(err) {
+    server.on('error', function (err) {
         done(err);
     });
 
     server.listen(port, 'localhost');
-
 };
--- test.js
+++ test.js
@@ -1,64 +1,60 @@
-var porter = require('./index')
-    , net = require('net')
-    , async = require('async')
-    , expect = require('chai').expect
-;
+var net = require('net');
+var async = require('async');
+var expect = require('chai').expect;
 
-describe('it should test library', function() {
+var porter = require('./index');
+
+describe('it should test library', function () {
     var busyPort, freePort, server;
 
-    before(function(done) {
+    before(function (done) {
         async.series([
-
-            function(done_) {
+            function (done_) {
                 server = net.createServer();
-                server.on('listening', function() {
+                server.on('listening', function () {
                     busyPort = server.address().port;
                     done_();
                 });
                 server.listen();
             },
-
-            function(done_) {
+            function (done_) {
                 var freePortServer = net.createServer();
-                freePortServer.on('listening', function() {
+                freePortServer.on('listening', function () {
                     freePort = freePortServer.address().port;
                     freePortServer.close();
                     done_();
                 });
                 freePortServer.listen();
             }
-
         ], done);
     });
 
-    it('should fail when trying to probe used port', function(done) {
-        porter(busyPort, function(err, port) {
+    it('should fail when trying to probe used port', function (done) {
+        porter(busyPort, function (err, port) {
             expect(err).to.not.be.undefined;
             done();
         });
     });
 
-    it('should succeed to open some available port', function(done) {
-        porter(freePort, function(err, port) {
+    it('should succeed to open some available port', function (done) {
+        porter(freePort, function (err, port) {
             expect(err).to.be.undefined;
             expect(port).to.equal(freePort);
             done();
         });
     });
 
-    it('should succed to open random port', function(done) {
-        porter(function(err, port) {
+    it('should succed to open random port', function (done) {
+        porter(function (err, port) {
             expect(err).to.be.undefined;
             expect(port).to.be.above(0);
             done();
         });
-    })
+    });
 
-    after(function(done) {
-        server.close(function() {
+    after(function (done) {
+        server.close(function () {
             done();
         });
     });
-
 });
